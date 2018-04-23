78a620f324bc645b610ab6be7cd715fbb7d70275
Camlee8 <clm0070@yahoo.com>
2018-04-23 17:00:20 -0500

    The beginning of the UI code.

---
+++ TestProjecctUI/TestProjecctUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjecctUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The following is the show Hello World to the end user.
+            Console.WriteLine("Hello World!!!!");
+
+        }
+    }
+}
